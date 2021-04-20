7e6de4961f821e921f0cf0b6c0633d6921e5318f
Felipe Rovaris Barros <felipeknight_98@yahoo.com>
2021-04-19 21:40:20 -0300

    Adding essential content to start the kit mechanic development.

---
+++ Assets/Scripts/KiteController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class KiteController : MonoBehaviour
+{
+    [SerializeField] private Transform kiteTransform;
+    private Transform _playerTransform;
+    private float _speed = 0.1f;
+
+    private void Awake()
+    {
+        _playerTransform = GetComponent<Transform>();
+    }
+}
