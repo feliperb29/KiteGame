59490f17948ad74dbb2d1c50deb6543ebd37bf52
feliperb29 <felipeknight_98@yahoo.com>
2021-04-21 17:00:05 -0300

    First attempt to make a kite controller, the kite can move with certain speed limiter, but dont respect the player line direction yet. The movement is not great, but it is a start.

--- Assets/Scripts/KiteController.cs
+++ Assets/Scripts/KiteController.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class KiteController : MonoBehaviour
 {
-    [SerializeField] private Transform kiteTransform;
-    private Transform _playerTransform;
-    private float _speed = 0.1f;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
 
-    private void Awake()
+    private Rigidbody _rb;
+    private Vector2 _movement;
+    
+    private void Awake() => _rb = GetComponent<Rigidbody>();
+
+    private void Update()
     {
-        _playerTransform = GetComponent<Transform>();
+        Debug.Log(Input.GetAxis("Horizontal"));
+        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    } 
+
+    private void FixedUpdate()
+    {
+        
+        MoveKite(_movement);
+    }
+
+    private void MoveKite(Vector2 direction)
+    {
+        _rb.AddForce(direction * speed);
+        
+        if (_rb.velocity.magnitude > maxSpeed)
+            _rb.velocity = _rb.velocity.normalized * maxSpeed;
     }
 }
